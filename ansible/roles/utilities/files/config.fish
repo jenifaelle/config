c14b5f45952cb0256fc8869448f90e0e815e9fa2
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2018-05-04 13:29:08 -0400

    Fix diverses, entretient

--- ansible/roles/utilities/files/config.fish
+++ ansible/roles/utilities/files/config.fish
@@ -1,5 +1,5 @@
 set -x fish_key_bindings fish_vi_key_bindings
-set -x BROWSER chromium
+set -x BROWSER firefox
 function fish_prompt
     ~/.config/fish/powerline-shell.py $status --shell bare ^/dev/null
 end
