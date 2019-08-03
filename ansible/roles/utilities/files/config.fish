1d5202fe855cc59b17fc05e75a6d56eec01fa367
Jenifael Gingras <jenifael.gingras@gmail.com>
2019-08-02 21:05:31 -0400

    feat(vim): more python sheananigans

--- ansible/roles/utilities/files/config.fish
+++ ansible/roles/utilities/files/config.fish
@@ -1,5 +1,6 @@
 set -x fish_key_bindings fish_vi_key_bindings
 set -x BROWSER firefox
+set -gx PATH $PATH /home/jenifael/bin
 function fish_prompt
     ~/.config/fish/powerline-shell.py $status --shell bare ^/dev/null
 end
