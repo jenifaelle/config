212177cccb7ff1eba54a78a9766cd731f62f350d
ginjeni1 <jenifael.gingras-courchesne@optelgroup.com>
2020-08-10 11:48:00 -0400

    feat: keepassxc

--- ansible/roles/utilities/files/config.fish
+++ ansible/roles/utilities/files/config.fish
@@ -3,11 +3,16 @@ set -x BROWSER firefox
 set -gx PATH $PATH /home/jenifael/bin
 set -gx PATH $PATH /home/jenifael/.roswell/bin
 set -x FZF_DEFAULT_COMMAND 'rg --hidden -l ""'
-set -x OPTELBRIDGE_DEPLOYER_GENERAL_PATH "/mnt/general/"
 set -x GITLAB_TOKEN "SBT2qisBwHkzwXarZBDS"
 set -x DJANGO_SETTINGS_MODULE optel_osm_application.settings.dev
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 set -x TERM screen-256color
+
+set -x OPTELBRIDGE_DEPLOYER_GENERAL_PATH "/mnt/general/"
+set -x OPTELBRIDGE_HOME "/opt/optelbridge"
+set -x BRIDGE_LOGS_PATH "/var/log/optelbridge"
+set -x BRIDGE_SERIAL_BACKUP_LOCATION "/opt/optelbridge"
+
 
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
